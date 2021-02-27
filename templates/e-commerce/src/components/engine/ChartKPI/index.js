f8073f75cec3e9f43c88d3db114050a38bf58df4
alangrn <alangrn@hotmail.com>
2021-02-27 22:47:32 +0000

    Chart animation corrected

--- templates/e-commerce/src/components/engine/ChartKPI/index.js
+++ templates/e-commerce/src/components/engine/ChartKPI/index.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Skeleton } from "antd";
 import useData from "../../../dev-resources/hooks/useData";
 
 import Widget from "dev-resources/components/Widget";
 import AreaChart from "components/engine/AreaChart";
 
 const ChartKPI = ({ dataProps }) => {
+  const [selected, setSelected] = useState(false);
   const { data, icon, styleName, chart } = dataProps;
 
   const { cols, qTitle, qMetrics } = data;
@@ -49,29 +51,41 @@ const ChartKPI = ({ dataProps }) => {
     // qSuppressZero: true,
   });
 
+  useEffect(() => {
+    if (!mData) return;
+
+    setSelected(true);
+  }, [mData]);
+
   return (
-    <Widget styleName="gx-card-full">
-      {metrics && (
-        <div className="gx-actchart gx-px-3 gx-pt-3">
-          <div className="ant-row-flex">
-            <h2 className="gx-mb-0 gx-fs-xxl gx-font-weight-medium">
-              {metrics.prize}
-              <span
-                className={`gx-mb-0 gx-ml-2 gx-pt-xl-2 gx-fs-lg gx-chart-${styleName}`}
-              >
-                {metrics.desc} <i className="icon icon-menu-up gx-fs-sm" />
-              </span>
-            </h2>
-            <i
-              className={`icon icon-${icon} gx-fs-xl gx-ml-auto gx-text-primary gx-fs-xxxl`}
-            />
-          </div>
-          <p className="gx-mb-0 gx-fs-sm gx-text-grey">{title}</p>
-        </div>
+    <>
+      {selected ? (
+        <Widget styleName="gx-card-full">
+          {metrics && (
+            <div className="gx-actchart gx-px-3 gx-pt-3">
+              <div className="ant-row-flex">
+                <h2 className="gx-mb-0 gx-fs-xxl gx-font-weight-medium">
+                  {metrics.prize}
+                  <span
+                    className={`gx-mb-0 gx-ml-2 gx-pt-xl-2 gx-fs-lg gx-chart-${styleName}`}
+                  >
+                    {metrics.desc} <i className="icon icon-menu-up gx-fs-sm" />
+                  </span>
+                </h2>
+                <i
+                  className={`icon icon-${icon} gx-fs-xl gx-ml-auto gx-text-primary gx-fs-xxxl`}
+                />
+              </div>
+              <p className="gx-mb-0 gx-fs-sm gx-text-grey">{title}</p>
+            </div>
+          )}
+          {/* {React.cloneElement(children, { data: mData })} */}
+          <Chart data={mData} margin={margin} height={height} />
+        </Widget>
+      ) : (
+        <Skeleton active />
       )}
-      {/* {React.cloneElement(children, { data: mData })} */}
-      <Chart data={mData} margin={margin} height={height} />
-    </Widget>
+    </>
   );
 };
 
