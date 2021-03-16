e511a98095adffad6074e4b8d576bd6e54934b86
lureilly1 <lureilly1@gmail.com>
2021-03-16 09:36:10 +0000

    fix Motor Search

--- templates/dev-template/src/components/engine/MotorSearch/index.js
+++ templates/dev-template/src/components/engine/MotorSearch/index.js
@@ -1,33 +1,13 @@
-import React, { useState } from "react";
 import React, { useState, useEffect } from "react";
 import { Input, AutoComplete } from 'antd';
 //import searchResults from './searchResults'
-import useSearch from '../../../dev-resources/hooks/useSearch'
-
-function getRandomInt(max, min = 0) {
-  return Math.floor(Math.random() * (max - min + 1)) + min; // eslint-disable-line no-mixed-operators
-}
-
-const searchResult = (query) =>
-  new Array(getRandomInt(10000))
-    .join('.')
-    .split('.')
-    .map((_, idx) => {
-      const category = `${query}${idx}`;
-      return {
-        value: category
-      };
-    });
+import { useSearch } from "@motor-js/engine"
 
 const MotorSearch = () => {
 
   const [options, setOptions] = useState([]);
-
-  const { searchResults, select } = useSearch()
-
-  console.log(searchResults)
-
   const [searchValue, setSearchValue] = useState("");
+
   const qCount = 100;
   const qGroupItemCount = 100;
 
@@ -40,15 +20,11 @@ const MotorSearch = () => {
     qCount,
     qGroupItemCount
   })
-  console.log(groupedResults)
+
   const handleSearch = (value) => {
-    setOptions(value ? searchResult(value) : []);
-    console.log(searchResult(value))
     setSearchValue(value)
   };
 
-  const onSelect = (value) => {
-    console.log('onSelect', value);
   const onSelect = (val, obj) => {
     flatSelect(obj.dimension,val)
   };
@@ -70,4 +46,4 @@ const MotorSearch = () => {
   )
 }
 
-export default MotorSearch
+export default MotorSearch;
