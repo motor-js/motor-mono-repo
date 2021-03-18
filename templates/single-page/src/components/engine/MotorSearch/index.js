a8d4cfb871ee3ba04c6d2886c3b8560ad12f1d94
alangrn <alangrn@hotmail.com>
2021-03-18 07:25:48 +0000

    singel page setup

--- templates/single-page/src/components/engine/MotorSearch/index.js
+++ templates/single-page/src/components/engine/MotorSearch/index.js
@@ -1,43 +1,32 @@
 import React, { useState, useEffect } from "react";
-import { Input, AutoComplete } from 'antd';
+import { Input, AutoComplete } from "antd";
 //import searchResults from './searchResults'
-import { useSearch } from "@motor-js/engine"
+import { useSearch } from "@motor-js/engine";
 
-<<<<<<< HEAD
-const MotorSearch = () => {
-=======
-const MotorSearch = ({
-  styleName,
-  placeholder,
-}) => {
->>>>>>> be2b8f5b0ecc492977ca2f3f8e1b261411673e0d
-
+const MotorSearch = ({ styleName, placeholder }) => {
   const [options, setOptions] = useState([]);
   const [searchValue, setSearchValue] = useState("");
 
   const qCount = 100;
   const qGroupItemCount = 100;
 
-  const { 
-    flatResults,
-    flatSelect,
-  } = useSearch({ 
+  const { flatResults, flatSelect } = useSearch({
     searchValue,
     qCount,
-    qGroupItemCount
-  })
+    qGroupItemCount,
+  });
 
   const handleSearch = (value) => {
-    setSearchValue(value)
+    setSearchValue(value);
   };
 
   const onSelect = (val, obj) => {
-    flatSelect(obj.dimension,val)
+    flatSelect(obj.dimension, val);
   };
 
   useEffect(() => {
-    setOptions(flatResults)
-  },[flatResults])
+    setOptions(flatResults);
+  }, [flatResults]);
 
   return (
     <div className={`gx-search-bar ${styleName}`}>
@@ -51,7 +40,7 @@ const MotorSearch = ({
         <Input.Search size="medium" placeholder={placeholder} />
       </AutoComplete>
     </div>
-  )
-}
+  );
+};
 
 export default MotorSearch;
--- templates/single-page/src/containers/Topbar.js
+++ templates/single-page/src/containers/Topbar.js
@@ -35,7 +35,7 @@ const Topbar = () => {
 
   return (
     <Header>
-<<<<<<< HEAD
+      {/* <div className="gx-header-horizontal-main-flex">*/}
       {themeState.theme === "light" ? (
         <img
           src={logoLight}
@@ -99,77 +99,11 @@ const Topbar = () => {
           </Popover>
         </li>
         {showThemeSwitch && (
-=======
-     {/* <div className="gx-header-horizontal-main-flex">*/}
-          {themeState.theme === "light" ? (
-            <img
-              src={logoLight}
-              alt="Logo"
-              style={{ height: "40px", width: "120px" }}
-            />
-          ) : (
-            <img
-              src={logoDark}
-              alt="Logo"
-              style={{ height: "40px", width: "120px" }}
-            />
-          )}
-        <ul className="gx-header-notifications">
-          <li className="gx-nav-icon gx-ml-auto">
-            <MotorSearch
-              styleName="gx-d-none gx-d-lg-block gx-lt-icon-search-bar-lg"
-              placeholder="Search in app..."
-              onChange={handleSearch}
-              value={searchText}
-            />
-          </li>
-          <li className="gx-notify gx-notify-search gx-d-inline-block gx-d-lg-none">
-            <Popover
-              overlayClassName="gx-popover-horizantal"
-              placement="bottomRight"
-              trigger="click"
-              content={
-                <div className="gx-d-flex">
-                  <MotorSearch
-                    styleName="gx-popover-search-bar"
-                    placeholder="Search in app..."
-                    onChange={handleSearch}
-                    value={searchText}
-                  />
-                </div>
-              }
-            >
-              <span className="gx-pointer gx-d-block">
-                <i className="icon icon-search-new" />
-              </span>
-            </Popover>
-          </li>
->>>>>>> be2b8f5b0ecc492977ca2f3f8e1b261411673e0d
           <li className="gx-nav-icon">
-            <Popover
-              overlayClassName="gx-popover-horizantal"
-              placement="bottomRight"
-              content={
-                <Selections selections={selections} handleClear={handleClear} />
-              }
-              trigger="click"
-            >
-              <span className="gx-pointer gx-status-pos gx-d-block">
-                <FilterOutlined style={{ fontSize: "18px" }} />
-                {selections && selections.length > 0 ? (
-                  <span className="gx-status gx-status-rtl gx-small gx-orange" />
-                ) : (
-                  <span className="gx-status gx-status-rtl gx-small" />
-                )}
-              </span>
-            </Popover>
+            <Switch onChange={handleSwitch} />
           </li>
-          {showThemeSwitch && (
-            <li className="gx-nav-icon">
-              <Switch onChange={handleSwitch} />
-            </li>
-          )}
-        </ul>
+        )}
+      </ul>
       {/*</div>*/}
     </Header>
   );
