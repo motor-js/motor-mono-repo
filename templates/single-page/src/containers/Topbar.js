d80af050fa757119beae98ac6efa97f1311ebb1d
alangrn <alangrn@hotmail.com>
2021-03-21 13:15:39 +0000

    Topbar continued

--- templates/single-page/src/containers/Topbar.js
+++ templates/single-page/src/containers/Topbar.js
@@ -1,11 +1,10 @@
 import React, { useState, useContext } from "react";
 import { Layout, Popover, Switch } from "antd";
-import FilterOutlined from "@ant-design/icons/lib/icons/FilterOutlined";
 import SelectOutlined from "@ant-design/icons/lib/icons/SelectOutlined";
 
 import Selections from "components/engine/Selections";
-import Filters from "components/engine/Filters";
 import MotorSearch from "components/engine/MotorSearch";
+import Filter from "components/engine/Filter";
 
 import { useSelections } from "@motor-js/engine";
 import { ThemeContext } from "store";
@@ -36,99 +35,105 @@ const Topbar = () => {
     });
 
   return (
-    <Header>
-      {/* <div className="gx-header-horizontal-main-flex">*/}
-      {themeState.theme === "light" ? (
-        <img
-          src={logoLight}
-          alt="Logo"
-          style={{ height: "40px", width: "120px" }}
-        />
-      ) : (
-        <img
-          src={logoDark}
-          alt="Logo"
-          style={{ height: "40px", width: "120px" }}
-        />
-      )}
-      <ul className="gx-header-notifications">
-        <li className="gx-nav-icon gx-ml-auto">
-          <MotorSearch
-            styleName="gx-d-none gx-d-lg-block gx-lt-icon-search-bar-lg"
-            placeholder="Search in app..."
-            onChange={handleSearch}
-            value={searchText}
+    <>
+      <Header>
+        {/* <div className="gx-header-horizontal-main-flex">*/}
+        {themeState.theme === "light" ? (
+          <img
+            src={logoLight}
+            alt="Logo"
+            style={{ height: "40px", width: "120px" }}
           />
-        </li>
-        <li className="gx-notify gx-notify-search gx-d-inline-block gx-d-lg-none">
-          <Popover
-            overlayClassName="gx-popover-horizantal"
-            placement="bottomRight"
-            trigger="click"
-            content={
-              <div className="gx-d-flex">
-                <MotorSearch
-                  styleName="gx-popover-search-bar"
-                  placeholder="Search in app..."
-                  onChange={handleSearch}
-                  value={searchText}
-                />
-              </div>
-            }
-          >
-            <span className="gx-pointer gx-d-block">
-              <i className="icon icon-search-new" />
-            </span>
-          </Popover>
-        </li>
-        <li className="gx-nav-icon">
-          <Popover
-            overlayClassName="gx-popover-horizantal"
-            placement="bottomRight"
-            content={
-              // <Selections selections={selections} handleClear={handleClear} />
-              // <MotorFilter dimension={["Country"]} />
-              <Filters />
-            }
-            trigger="click"
-          >
-            <span className="gx-pointer gx-status-pos gx-d-block">
-              <FilterOutlined style={{ fontSize: "18px" }} />
-              {selections && selections.length > 0 ? (
-                <span className="gx-status gx-status-rtl gx-small gx-orange" />
-              ) : (
-                <span className="gx-status gx-status-rtl gx-small" />
-              )}
-            </span>
-          </Popover>
-        </li>
-        <li className="gx-nav-icon">
-          <Popover
-            overlayClassName="gx-popover-horizantal"
-            placement="bottomRight"
-            content={
-              <Selections selections={selections} handleClear={handleClear} />
-            }
-            trigger="click"
-          >
-            <span className="gx-pointer gx-status-pos gx-d-block">
-              <SelectOutlined style={{ fontSize: "18px" }} />
-              {selections && selections.length > 0 ? (
-                <span className="gx-status gx-status-rtl gx-small gx-orange" />
-              ) : (
-                <span className="gx-status gx-status-rtl gx-small" />
-              )}
-            </span>
-          </Popover>
-        </li>
-        {showThemeSwitch && (
-          <li className="gx-nav-icon">
-            <Switch onChange={handleSwitch} />
-          </li>
+        ) : (
+          <img
+            src={logoDark}
+            alt="Logo"
+            style={{ height: "40px", width: "120px" }}
+          />
         )}
-      </ul>
-      {/*</div>*/}
-    </Header>
+        <ul className="gx-header-notifications">
+          <li className="gx-nav-icon gx-ml-auto">
+            <MotorSearch
+              styleName="gx-d-none gx-d-lg-block gx-lt-icon-search-bar-lg"
+              placeholder="Search in app..."
+              onChange={handleSearch}
+              value={searchText}
+            />
+          </li>
+          <li className="gx-notify gx-notify-search gx-d-inline-block gx-d-lg-none">
+            <Popover
+              overlayClassName="gx-popover-horizantal"
+              placement="bottomRight"
+              trigger="click"
+              content={
+                <div className="gx-d-flex">
+                  <MotorSearch
+                    styleName="gx-popover-search-bar"
+                    placeholder="Search in app..."
+                    onChange={handleSearch}
+                    value={searchText}
+                  />
+                </div>
+              }
+            >
+              <span className="gx-pointer gx-d-block">
+                <i className="icon icon-search-new" />
+              </span>
+            </Popover>
+          </li>
+          {/* <li className="gx-nav-icon">
+            <Popover
+              overlayClassName="gx-popover-horizantal"
+              placement="bottomRight"
+              content={
+                // <Selections selections={selections} handleClear={handleClear} />
+                // <MotorFilter dimension={["Country"]} />
+                <Filters />
+              }
+              trigger="click"
+            >
+              <span className="gx-pointer gx-status-pos gx-d-block">
+                <FilterOutlined style={{ fontSize: "18px" }} />
+                {selections && selections.length > 0 ? (
+                  <span className="gx-status gx-status-rtl gx-small gx-orange" />
+                ) : (
+                  <span className="gx-status gx-status-rtl gx-small" />
+                )}
+              </span>
+            </Popover>
+          </li> */}
+          <li className="gx-nav-icon">
+            <Popover
+              overlayClassName="gx-popover-horizantal"
+              placement="bottomRight"
+              content={
+                <Selections selections={selections} handleClear={handleClear} />
+              }
+              trigger="click"
+            >
+              <span className="gx-pointer gx-status-pos gx-d-block">
+                <SelectOutlined style={{ fontSize: "18px" }} />
+                {selections && selections.length > 0 ? (
+                  <span className="gx-status gx-status-rtl gx-small gx-orange" />
+                ) : (
+                  <span className="gx-status gx-status-rtl gx-small" />
+                )}
+              </span>
+            </Popover>
+          </li>
+          {showThemeSwitch && (
+            <li className="gx-nav-icon">
+              <Switch onChange={handleSwitch} />
+            </li>
+          )}
+        </ul>
+        {/*</div>*/}
+      </Header>
+      <Header>
+        <Filter dimension={["Country"]} style={{ width: "50%" }} />
+        <Filter dimension={["City"]} />
+      </Header>
+    </>
   );
 };
 
