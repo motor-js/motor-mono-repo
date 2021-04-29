ab6e35061e63458693310866d119f47bc707c72a
lureilly1 <lureilly1@gmail.com>
2021-04-29 12:51:53 +0100

    update settings

--- templates/single-page/src/settings/index.js
+++ templates/single-page/src/settings/index.js
@@ -2,31 +2,9 @@ export const footer = {
   footerText: "Copyright Motor © 2021",
 };
 
-/*
 export const qlikConfig = {
   //Enter your app config here..
-  host: "localhost", // Qlik Sense Host
-  secure: false, // Whether your host is secure of not (HTTPS / HTTP)
-  port: 19076, // Qlik Sense site port
-  prefix: "", // Prefix
-  appId: "Wearables2", // Application Id
-};
-*/
-
-export const qlikConfig = {
-  //Enter your app config here..
-  host: "motor-demo.com", // Qlik Sense Host
-  secure: true, // Whether your host is secure of not (HTTPS / HTTP)
-  port: 443, // Qlik Sense site port
-  prefix: "", // Prefix
-  appId: "Wearables2", // Application Id
-};
-
-
-/*
-export const qlikConfig = {
-  //Enter your app config here..
-  host: "juno-ui.eu.qlikcloud.com", // Qlik Sense Host
+  host: "motor.eu.qlikcloud.com", // Qlik Sense Host
   secure: true, // Whether your host is secure of not (HTTPS / HTTP)
   port: null, // Qlik Sense site port
   prefix: "", // Prefix
@@ -34,7 +12,7 @@ export const qlikConfig = {
   webIntId: "4Tx-ydWxSQEM_q1ajlYBVzGgVUVJUo-i", // Web Integration Id, for connection to Qlik cloud
   qcs: true, // whether you are connecting to a Qlik Cloud site or not
 };
-*/
+
 
 export const appSettings = {
   theme: "light", // light or dark
@@ -42,7 +20,6 @@ export const appSettings = {
   primaryColor: "", // Primary color for Login & Not Connected Components,
 
   /* For the settings below, note that the login component is only available when connecting to Qlik cloud */
-
   logo: "/static/media/motor-red.99524ab9.png", // Path to logo used for the Login box
   logoHeight: "40px", // Login logo height
   logoWidth: "120px", // Login logo width
