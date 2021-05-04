0a5d89539039fc6b8d88805829d5189ae14a76dc
lureilly1 <lureilly1@gmail.com>
2021-05-04 14:50:24 +0100

    rebuild

--- packages/engine/src/index.ts
+++ packages/engine/src/index.ts
@@ -2,6 +2,7 @@
 export { default as Motor } from "./components/Motor";
 export { default as Login } from "./components/Login";
 export { default as NotConnected } from "./components/NotConnected";
+
 //Hooks
 export { default as useEngine } from "./hooks/useEngine";
 export { default as useHyperCube } from "./hooks/useHyperCube";
@@ -13,5 +14,6 @@ export { default as useSearch } from "./hooks/useSearch";
 export { default as useSelections } from "./hooks/useSelections";
 export { default as useCapability } from "./hooks/useCapability";
 export { default as useApp } from "./hooks/useApp"
+
 //Contexts
 export { EngineContext } from "./contexts/EngineProvider";
