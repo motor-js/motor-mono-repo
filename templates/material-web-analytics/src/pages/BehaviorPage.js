442221357256fe4d9c3b025623df522d46a5da95
alangrn <alangrn@hotmail.com>
2021-05-17 18:04:11 +0100

    Line on Behaviour Page

--- templates/material-web-analytics/src/pages/BehaviorPage.js
+++ templates/material-web-analytics/src/pages/BehaviorPage.js
@@ -1,10 +1,27 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
-// import OverTimeChart from "../components/OverTimeChart";
+import OverTimeChart from "../components/OverTimeChart";
 import Chart from "../components/Chart";
 // import SwitchTable from "../components/SwitchTable";
 
 const queries = {
+  pageviewsOverTime: {
+    chartType: "line",
+    cols: [
+      {
+        qField: "[Period]",
+        qLabel: "Period",
+      },
+      {
+        qField: "=count(Dim1)",
+        qLabel: "Session Users",
+        // useFormatting: true,
+        // qNumType: "M",
+        // qNumFmt: "£#,##0",
+      },
+    ],
+    granularity: "day",
+  },
   pageviews: {
     chartType: "number",
     cols: [],
@@ -55,10 +72,14 @@ const BehaviorPage = ({ withTime }) => (
   <Grid item xs={12}>
     <Grid container spacing={3}>
       <Grid item xs={12}>
-        {/* <OverTimeChart
+        <OverTimeChart
           title="Pageviews"
-          vizState={withTime(queries.pageviewsOverTime)}
-        /> */}
+          vizState={withTime({
+            chartType: "line",
+            legend: false,
+            query: withTime(queries.pageviewsOverTime),
+          })}
+        />
       </Grid>
       <Grid item xs={2}>
         <Chart title="Pageviews" vizState={withTime(queries.pageviews)} />
