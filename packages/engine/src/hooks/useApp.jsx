02ec87edb640f2cf2c6a1d2f08d54b575b5335d1
alangrn <alangrn@hotmail.com>
2021-05-27 20:22:01 +0100

    useApp dev start

--- packages/engine/src/hooks/useApp.jsx
+++ packages/engine/src/hooks/useApp.jsx
@@ -2,22 +2,30 @@ import { useContext, useState, useEffect } from "react";
 import { EngineContext } from "../contexts/EngineProvider";
 
 const useApp = () => {
-
   const { engine } = useContext(EngineContext) || {};
-  const [app, setApp] = useState(null)
+  const [app, setApp] = useState(null);
+  const [appLayout, setAppLayout] = useState(null);
+  const [appName, setAppName] = useState(null);
+  const [lastReloadTime, setLastRelaodTime] = useState(null);
 
-  useEffect(() => (
-    (async () => {
-      if (engine === undefined) {
-      } else {
-      const qDoc = await engine;
-      console.log('qDoc',qDoc)
-      setApp(qDoc)
-      }
-    })()
-  ), [engine]);
-  
-  return { app }
-}
+  useEffect(
+    () =>
+      (async () => {
+        if (engine === undefined) {
+        } else {
+          const qDoc = await engine;
+          const qAppLayout = await qDoc.getAppLayout();
+          // console.log('qDoc',qDoc)
+          setApp(qDoc);
+          setAppLayout(qAppLayout);
+          setAppName(qAppLayout.qTitle);
+          setLastRelaodTime(qAppLayout.qLastReloadTime);
+        }
+      })(),
+    [engine]
+  );
 
-export default useApp
+  return { app, appLayout, appName, lastReloadTime };
+};
+
+export default useApp;
