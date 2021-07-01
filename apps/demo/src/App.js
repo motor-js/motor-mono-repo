58f15a21cc5dd2143463c484de98469a9fadb819
lureilly1 <lureilly1@gmail.com>
2021-07-01 16:16:05 +0100

    app test

--- apps/demo/src/App.js
+++ apps/demo/src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
 import Table from "./components/TableExampleCompact";
 import ButtonComponent from "./components/ButtonComponent";
+import { useList } from "@motor-js/engine"
 
 export default function App() {
 
-  /*
+  const dimension = ['Company Name'];
+
+  const {
+    listData
+  } = useList({
+    dimension,
+  });
+
+console.log(listData);
+
+
+/*
+
 //const items = createLicense()
 // Array of MotorLicense1,2,3 ....
 
@@ -19,6 +32,7 @@ var decrypt = CryptoAES.decrypt(encrypt.toString(), "S@few/M0t0r");
 //var decrypt = CryptoAES.decrypt('U2FsdGVkX18F0eIS4Rkz49TnlMpqP3Yfos8sRJ/GvNY=', "S@few/M0t0r");
 console.log(decrypt.toString(CryptoENC))
 console.log(decrypt.toString(CryptoJS.enc.Hex))
+
 */
 
   return (
