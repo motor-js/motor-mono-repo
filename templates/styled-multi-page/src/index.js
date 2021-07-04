a449e57a81c47d88eb059b570d31dfdfbf1084a0
alangrn <alangrn@hotmail.com>
2021-07-04 11:35:01 +0100

    initial render

--- templates/styled-multi-page/src/index.js
+++ templates/styled-multi-page/src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { ThemeProvider, theme } from "./styled";
+import { GlobalStyle } from "./css";
+import { store } from "./redux/store";
+import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </Provider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- templates/styled-multi-page/src/redux/store.js
+++ templates/styled-multi-page/src/redux/store.js
@@ -4,16 +4,11 @@ import uiReducer from "./slices/ui";
 import chatUISlice from "./slices/chat-ui";
 
 const rootReducer = combineReducers({
-    events: eventReducer,
-    ui: uiReducer,
-    chatUI: chatUISlice,
+  events: eventReducer,
+  ui: uiReducer,
+  chatUI: chatUISlice,
 });
 
 export const store = configureStore({
-    reducer: rootReducer,
+  reducer: rootReducer,
 });
-
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch;
