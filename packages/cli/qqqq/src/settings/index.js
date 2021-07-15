6978af819ae8cb96881ebe5ee390fa8cf30d4390
lureilly1 <lureilly1@gmail.com>
2021-07-15 17:04:22 +0100

    Secondary commit from Create Frontend App

--- packages/cli/qqqq/src/settings/index.js
+++ packages/cli/qqqq/src/settings/index.js
@@ -1,11 +1,11 @@
 export const qlikConfig = {
   //Enter your app config here..
-  host: "test", // Qlik Sense Host
+  host: "wwwwwww", // Qlik Sense Host
   secure: true, // Whether your host is secure of not (HTTPS / HTTP)
   port: null, // Qlik Sense site port
   prefix: "", // Prefix
-  appId: "0294cf88-eb02-484a-b315-cf06b45ac347", // Application Id
-  webIntId: "4Tx-ydWxSQEM_q1ajlYBVzGgVUVJUo-i", // Web Integration Id, for connection to Qlik cloud
+  appId: "qqqqqq", //"0294cf88-eb02-484a-b315-cf06b45ac347" Application Id
+  webIntId: "eeeeeee", //"4Tx-ydWxSQEM_q1ajlYBVzGgVUVJUo-i" Web Integration Id, for connection to Qlik cloud
   qcs: true, // whether you are connecting to a Qlik Cloud site or not
 };
 
