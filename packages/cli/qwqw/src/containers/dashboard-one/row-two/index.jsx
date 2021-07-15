3f7c930fc6df42b84cd3838b920a7a8feb6b657f
lureilly1 <lureilly1@gmail.com>
2021-07-15 16:22:49 +0100

    Secondary commit from Create Frontend App

---
+++ packages/cli/qwqw/src/containers/dashboard-one/row-two/index.jsx
@@ -0,0 +1,18 @@
+import { Col } from "../../../components/grid/grid";
+import Chart from "../../../components/dashboard-one/chart-placeholder"
+
+const RowTwo = () => {
+
+  return (
+    <>
+      <Col lg={8} xl={7} mt="10px">
+        <Chart />
+      </Col>
+      <Col lg={4} xl={5} mt="10px">
+        <Chart />
+      </Col>
+    </>
+  );
+};
+
+export default RowTwo;
