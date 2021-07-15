8cb7e5136603c52a3a482da722442b2349884e7a
lureilly1 <lureilly1@gmail.com>
2021-07-15 17:32:11 +0100

    Secondary commit from Create Frontend App

---
+++ packages/cli/qwwww/src/components/header/nav-search/index.jsx
@@ -0,0 +1,72 @@
+import { Search, X } from "react-feather";
+import { Input } from "../../forms/form-elements";
+import {
+  StyledSearch,
+  StyledSearchHeader,
+  StyledSearchBtn,
+  StyledSearchClose,
+  StyledSearchBody,
+  StyledSearchTitle,
+  StyledNavList,
+  StyledNavBtn,
+  StyledNavDevider,
+  StyledNavListItem,
+} from "./style";
+
+const NavSearch = ({ isOpen, onClose }) => {
+  return (
+    <StyledSearch $isOpen={isOpen}>
+      <StyledSearchHeader>
+        <Input
+          type="search"
+          id="nav-search"
+          name="nav-search"
+          placeholder="Type and hit enter to search..."
+          customStyle="noborder"
+          fontSize={["14px", null, null, "16px"]}
+        />
+        <StyledSearchBtn variant="texted" color="light">
+          <Search />
+        </StyledSearchBtn>
+        <StyledSearchClose variant="texted" onClick={onClose}>
+          <X />
+        </StyledSearchClose>
+      </StyledSearchHeader>
+      <StyledSearchBody>
+        <StyledSearchTitle>Recent Searches</StyledSearchTitle>
+        <StyledNavList>
+          <StyledNavListItem>
+            <StyledNavBtn href="#">modern dashboard</StyledNavBtn>
+          </StyledNavListItem>
+          <StyledNavListItem>
+            <StyledNavBtn href="#">calendar app</StyledNavBtn>
+          </StyledNavListItem>
+          <StyledNavListItem>
+            <StyledNavBtn href="#">modal examples</StyledNavBtn>
+          </StyledNavListItem>
+          <StyledNavListItem>
+            <StyledNavBtn href="#">avatar</StyledNavBtn>
+          </StyledNavListItem>
+        </StyledNavList>
+        <StyledNavDevider />
+        <StyledSearchTitle>Search Suggestions</StyledSearchTitle>
+        <StyledNavList>
+          <StyledNavListItem>
+            <StyledNavBtn href="#">cryptocurrency</StyledNavBtn>
+          </StyledNavListItem>
+          <StyledNavListItem>
+            <StyledNavBtn href="#">button groups</StyledNavBtn>
+          </StyledNavListItem>
+          <StyledNavListItem>
+            <StyledNavBtn href="#">form elements</StyledNavBtn>
+          </StyledNavListItem>
+          <StyledNavListItem>
+            <StyledNavBtn href="#">contact app</StyledNavBtn>
+          </StyledNavListItem>
+        </StyledNavList>
+      </StyledSearchBody>
+    </StyledSearch>
+  );
+};
+
+export default NavSearch;
