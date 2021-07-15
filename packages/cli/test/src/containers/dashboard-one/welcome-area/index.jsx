0668ad6146cce301dc475e1e0b0cb40c4e0b383d
lureilly1 <lureilly1@gmail.com>
2021-07-15 10:29:08 +0100

    Secondary commit from Create Frontend App

---
+++ packages/cli/test/src/containers/dashboard-one/welcome-area/index.jsx
@@ -0,0 +1,27 @@
+import Breadcrumb from "../../../components/breadcrumb";
+import {
+  StyledWelcomeArea,
+  StyledWelcomeLeft,
+  StyledWelcomeRight,
+} from "./style";
+
+const WelcomeArea = () => {
+
+  return (
+    <>
+      <StyledWelcomeArea>
+        <StyledWelcomeLeft>
+          <Breadcrumb
+            prev={[{ text: "Group 1", link: "/" }]}
+            title="Dashboard 1"
+            wcText="Welcome"
+          />
+        </StyledWelcomeLeft>
+        <StyledWelcomeRight>
+        </StyledWelcomeRight>
+      </StyledWelcomeArea>
+    </>
+  );
+};
+
+export default WelcomeArea;
