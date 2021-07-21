a9bd073fcb625db68272b8ed3382587117a83861
alan green <alangrn@hotmail.com>
2021-07-21 06:46:57 +0100

    ticketChart connected to Qlik

--- templates/styled-multi-page/src/components/dashboard-four/ticket-request/index.jsx
+++ templates/styled-multi-page/src/components/dashboard-four/ticket-request/index.jsx
@@ -7,83 +7,120 @@ import {
 // import { ticketRequestChart } from "../../data/dashboard-four";
 import { StyledHeader, StyledChart } from "./style";
 
-const ticketRequestChart = {
-  data: {
-    labels: [
-      "Modification",
-      "Code Request",
-      "Feature Request",
-      "Bug Fix",
-      "Integration",
-      "Production",
-    ],
-    datasets: [
-      {
-        data: [90, 60, 50, 95, 50, 60],
-        backgroundColor: [
-          "#65e0e0",
-          "#69b2f8",
-          "#6fd39b",
-          "#f77eb9",
-          "#fdb16d",
-          "#c693f9",
+const TicketRequest = () => {
+  // const cols = [
+  //   {
+  //     qField: "Date",
+  //     qLabel: "Date",
+  //   },
+
+  //   {
+  //     qField: "=sum([New Tickets])",
+  //     qLabel: "New Tickets",
+  //   },
+  //   {
+  //     qField: "=sum([Solved Tickets])",
+  //     qLabel: "Solved Tickets",
+  //   },
+  //   {
+  //     qField: "=sum([Open Tickets])",
+  //     qLabel: "Open Tickets",
+  //   },
+  // ];
+
+  // const { dataSet } = useData({
+  //   cols,
+  // });
+
+  // const { data } = dataSet;
+
+  // const newTickets = data
+  //   ? data.map((n) => [parseInt(n["Date"]), n["New Tickets"]])
+  //   : [];
+
+  // const solvedTickets = data
+  //   ? data.map((n) => [parseInt(n["Date"]), n["Solved Tickets"]])
+  //   : [];
+
+  // const openTickets = data
+  //   ? data.map((n) => [parseInt(n["Date"]), n["Open Tickets"]])
+  //   : [];
+
+  const ticketRequestChart = {
+    data: {
+      labels: [
+        "Modification",
+        "Code Request",
+        "Feature Request",
+        "Bug Fix",
+        "Integration",
+        "Production",
+      ],
+      datasets: [
+        {
+          data: [90, 60, 50, 95, 50, 60],
+          backgroundColor: [
+            "#65e0e0",
+            "#69b2f8",
+            "#6fd39b",
+            "#f77eb9",
+            "#fdb16d",
+            "#c693f9",
+          ],
+        },
+        {
+          data: [60, 50, 70, 45, 70, 30],
+          backgroundColor: "#e5e9f2",
+        },
+      ],
+    },
+    options: {
+      maintainAspectRatio: false,
+      responsive: true,
+      legend: {
+        display: false,
+        labels: {
+          display: false,
+        },
+      },
+      layout: {
+        padding: {
+          left: 5,
+        },
+      },
+      scales: {
+        yAxes: [
+          {
+            gridLines: {
+              display: false,
+            },
+            barPercentage: 0.5,
+            ticks: {
+              beginAtZero: true,
+              fontSize: 13,
+              fontColor: "#182b49",
+              fontFamily: "IBM Plex Sans",
+            },
+          },
+        ],
+        xAxes: [
+          {
+            gridLines: {
+              color: "#e5e9f2",
+            },
+            ticks: {
+              beginAtZero: true,
+              fontSize: 10,
+              fontColor: "#182b49",
+              max: 100,
+            },
+          },
         ],
       },
-      {
-        data: [60, 50, 70, 45, 70, 30],
-        backgroundColor: "#e5e9f2",
-      },
-    ],
-  },
-  options: {
-    maintainAspectRatio: false,
-    responsive: true,
-    legend: {
-      display: false,
-      labels: {
-        display: false,
-      },
     },
-    layout: {
-      padding: {
-        left: 5,
-      },
-    },
-    scales: {
-      yAxes: [
-        {
-          gridLines: {
-            display: false,
-          },
-          barPercentage: 0.5,
-          ticks: {
-            beginAtZero: true,
-            fontSize: 13,
-            fontColor: "#182b49",
-            fontFamily: "IBM Plex Sans",
-          },
-        },
-      ],
-      xAxes: [
-        {
-          gridLines: {
-            color: "#e5e9f2",
-          },
-          ticks: {
-            beginAtZero: true,
-            fontSize: 10,
-            fontColor: "#182b49",
-            max: 100,
-          },
-        },
-      ],
-    },
-  },
-};
+  };
 
-console.log("ticketRequestChart", ticketRequestChart);
-
-const TicketRequest = () => {
+  console.log("ticketRequestChart", ticketRequestChart);
   return (
     <Card>
       <StyledHeader>
--- templates/styled-multi-page/src/components/data/dashboard-four/index.jsx
+++ templates/styled-multi-page/src/components/data/dashboard-four/index.jsx
@@ -1,4 +1,3 @@
-// import ticketChart from "./ticket-charts";
 // import ticketRequestChart from "./ticket-request-chart";
 // import RadialData from "./radial-data";
 // import complaintsData from "./complaints";
