83bb6c78c684f3ad0d1851ba4c8476393cfabda0
alan green <alangrn@hotmail.com>
2021-10-12 16:43:07 +0100

    commit2

--- templates/showcase-app-1/src/components/dialogs/BaseDialog.js
+++ templates/showcase-app-1/src/components/dialogs/BaseDialog.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react';
-import withStyles from '@material-ui/styles/withStyles';
-import Dialog from '@material-ui/core/Dialog';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import React, { Component } from "react";
+import withStyles from "@material-ui/styles/withStyles";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 
-const styles = theme => ({
+const styles = (theme) => ({
   container: {
     maxWidth: 600,
     flexGrow: 1,
-    textAlign: 'center',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center'
+    textAlign: "center",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
   },
   bottomMargin: {
-    marginBottom: theme.spacing(2)
-  }
+    marginBottom: theme.spacing(2),
+  },
 });
 
 class BaseDialog extends Component {
@@ -28,18 +28,16 @@ class BaseDialog extends Component {
         onClose={onClose}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
-        scroll='body'
+        scroll="body"
       >
         <DialogTitle id="alert-dialog-title"></DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
-            <div className={classes.container}>
-              {this.props.children}
-            </div>
+            <div className={classes.container}>{this.props.children}</div>
           </DialogContentText>
         </DialogContent>
       </Dialog>
-    )
+    );
   }
 }
 
