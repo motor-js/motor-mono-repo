53c2790d6ee80e8b4023ce526d8404d9b43ebb0a
alan green <alangrn@hotmail.com>
2021-10-13 16:22:29 +0100

    latest commit

--- templates/showcase-app-1/src/components/Main.js
+++ templates/showcase-app-1/src/components/Main.js
@@ -8,13 +8,44 @@ import Grid from "@material-ui/core/Grid";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import SimpleLineChart from "./SimpleLineChart";
-import Months from "./common/Months";
+// import Months from "./common/Months";
 
 import Topbar from "./Topbar";
 const numeral = require("numeral");
 numeral.defaultFormat("0,000");
 
 const backgroundShape = require("../images/shape.svg");
+
+const Months = [
+  "01 December 2018",
+  "01 January 2019",
+  "01 February 2019",
+  "01 March 2019",
+  "01 April 2019",
+  "01 May 2019",
+  "01 June 2019",
+  "01 July 2019",
+  "01 August 2019",
+  "01 September 2019",
+  "01 October 2019",
+  "01 November 2019",
+  "01 December 2019",
+  "01 January 2020",
+  "01 March 2020",
+  "01 April 2020",
+  "01 May 2020",
+  "01 June 2020",
+  "01 July 2020",
+  "01 August 2020",
+  "01 September 2020",
+  "01 October 2020",
+  "01 November 2020",
+  "01 December 2020",
+  "01 January 2020",
+  "01 February 2020",
+  "01 March 2020",
+  "01 April 2020",
+];
 
 const monthRange = Months;
 const monthlyPayment = 100;
