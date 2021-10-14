5a93e3dbcc99ff91f8dfa4df23808e2564117b56
alan green <alangrn@hotmail.com>
2021-10-14 18:51:11 +0100

    showcase 2 conencted to motor

--- templates/showcase-app-1/src/index.js
+++ templates/showcase-app-1/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 import { qlikConfig } from "./config";
 import { Motor } from "@motor-js/engine";
 
@@ -17,8 +16,3 @@ ReactDOM.render(
   </Motor>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
--- templates/showcase-app-2/src/index.js
+++ templates/showcase-app-2/src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { qlikConfig } from "./config";
+import { Motor } from "@motor-js/engine";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Motor
+    config={qlikConfig}
+    body={"Please login to get started 🎉 "}
+    bodySub={"Try user: demo@motor-js.io & password: MotorDemo!"}
+    licenseKey="U2FsdGVkX19vjmrWbhnS5zEHEGj2PxXnorHHCxZ55lg="
+  >
+    <App />
+  </Motor>,
+  document.getElementById("root")
+);
