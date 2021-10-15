dd025cebb7dfdf788bc00a3389b92db6b52b4846
alan green <alangrn@hotmail.com>
2021-10-15 08:40:59 +0100

    Convert main showcase to functional component

--- templates/showcase-app-2/src/components/Main.js
+++ templates/showcase-app-2/src/components/Main.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import withStyles from "@material-ui/styles/withStyles";
 import { withRouter } from "react-router-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -8,7 +8,6 @@ import Grid from "@material-ui/core/Grid";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import SimpleLineChart from "./SimpleLineChart";
-import Months from "./common/Months";
 
 import Topbar from "./Topbar";
 const numeral = require("numeral");
@@ -16,9 +15,55 @@ numeral.defaultFormat("0,000");
 
 const backgroundShape = require("../images/shape.svg");
 
+const Months = [
+  "01 December 2018",
+  "01 January 2019",
+  "01 February 2019",
+  "01 March 2019",
+  "01 April 2019",
+  "01 May 2019",
+  "01 June 2019",
+  "01 July 2019",
+  "01 August 2019",
+  "01 September 2019",
+  "01 October 2019",
+  "01 November 2019",
+  "01 December 2019",
+  "01 January 2020",
+  "01 March 2020",
+  "01 April 2020",
+  "01 May 2020",
+  "01 June 2020",
+  "01 July 2020",
+  "01 August 2020",
+  "01 September 2020",
+  "01 October 2020",
+  "01 November 2020",
+  "01 December 2020",
+  "01 January 2020",
+  "01 February 2020",
+  "01 March 2020",
+  "01 April 2020",
+];
+
 const monthRange = Months;
 const monthlyPayment = 100;
 const monthlyInterest = 10;
+
+// const cols = [
+//   {
+//     qField: "[categories]",
+//     qLabel: "categories",
+//   },
+//   {
+//     qField: "=sum(Series1)",
+//     qLabel: "Series1",
+//   },
+//   {
+//     qField: "=sum(Series2)",
+//     qLabel: "Series2",
+//   },
+// ];
 
 const data = Array.from({ length: 1 + 10 }, (value, i) => {
   const delayed = i < 10;
@@ -126,193 +171,165 @@ const styles = (theme) => ({
   },
 });
 
-class Main extends Component {
-  state = {
-    learnMoredialog: false,
-    getStartedDialog: false,
-  };
+function Main(props) {
+  const { classes } = props;
 
-  componentDidMount() {}
-
-  openDialog = (event) => {
-    this.setState({ learnMoredialog: true });
-  };
-
-  dialogClose = (event) => {
-    this.setState({ learnMoredialog: false });
-  };
-
-  openGetStartedDialog = (event) => {
-    this.setState({ getStartedDialog: true });
-  };
-
-  closeGetStartedDialog = (event) => {
-    this.setState({ getStartedDialog: false });
-  };
-
-  render() {
-    const { classes } = this.props;
-    return (
-      <React.Fragment>
-        <CssBaseline />
-        <Topbar />
-        <div className={classes.root}>
-          <Grid container justifyContent="center">
-            <Grid
-              spacing={4}
-              alignItems="center"
-              justifyContent="center"
-              container
-              className={classes.grid}
-            >
-              <Grid item xs={12} md={4}>
-                <Paper className={classes.paper}>
-                  <div className={classes.box}>
-                    <Typography
-                      style={{ textTransform: "uppercase" }}
-                      color="secondary"
-                      gutterBottom
-                    >
-                      First title
-                    </Typography>
-                    <Typography variant="body2" gutterBottom>
-                      A first title style <br /> with two lines
-                    </Typography>
-                  </div>
-                  <div style={{ display: "flex", justifyContent: "flex-end" }}>
-                    <Button
-                      color="primary"
-                      variant="contained"
-                      className={classes.actionButtom}
-                    >
-                      Learn more
-                    </Button>
-                  </div>
-                </Paper>
-              </Grid>
-              <Grid item xs={12} md={4}>
-                <Paper className={classes.paper}>
-                  <div className={classes.box}>
-                    <Typography
-                      style={{ textTransform: "uppercase" }}
-                      color="secondary"
-                      gutterBottom
-                    >
-                      Another box
-                    </Typography>
-                    <Typography variant="body1" gutterBottom>
-                      A default box
-                    </Typography>
-                  </div>
-                  <div style={{ display: "flex", justifyContent: "flex-end" }}>
-                    <Button
-                      color="primary"
-                      variant="contained"
-                      className={classes.actionButtom}
-                    >
-                      Learn more
-                    </Button>
-                  </div>
-                </Paper>
-              </Grid>
-              <Grid item xs={12} md={4}>
-                <Paper className={classes.paper}>
-                  <div className={classes.box}>
-                    <Typography
-                      style={{ textTransform: "uppercase" }}
-                      color="secondary"
-                      gutterBottom
-                    >
-                      A box with a carousel
-                    </Typography>
-                    <Typography variant="body1" gutterBottom>
-                      If you click in Getting Started, you will see a nice
-                      carousel
-                    </Typography>
-                  </div>
-                  <div className={classes.alignRight}>
-                    <Button
-                      onClick={this.openDialog}
-                      variant="outlined"
-                      className={classes.actionButtom}
-                    >
-                      Learn more
-                    </Button>
-                    <Button
-                      onClick={this.openGetStartedDialog}
-                      color="primary"
-                      variant="contained"
-                      className={classes.actionButtom}
-                    >
-                      Dashboard
-                    </Button>
-                  </div>
-                </Paper>
-              </Grid>
-              <Grid container item xs={12}>
-                <Grid item xs={12}>
-                  <Paper
-                    className={classes.paper}
-                    style={{ position: "relative" }}
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Topbar />
+      <div className={classes.root}>
+        <Grid container justifyContent="center">
+          <Grid
+            spacing={4}
+            alignItems="center"
+            justifyContent="center"
+            container
+            className={classes.grid}
+          >
+            <Grid item xs={12} md={4}>
+              <Paper className={classes.paper}>
+                <div className={classes.box}>
+                  <Typography
+                    style={{ textTransform: "uppercase" }}
+                    color="secondary"
+                    gutterBottom
                   >
-                    <div>
-                      <Typography variant="subtitle1" gutterBottom>
-                        Some details
-                      </Typography>
-                      <Typography variant="body1">
-                        Details about the graph
-                      </Typography>
-                      <div style={{ marginTop: 14, marginBottom: 14 }}>
-                        <div className={classes.inlining}>
-                          <Avatar className={classes.loanAvatar}></Avatar>
-                          <Typography
-                            className={classes.inlining}
-                            variant="subtitle2"
-                            gutterBottom
-                          >
-                            Type
-                          </Typography>
-                          <Typography
-                            className={classes.inlining}
-                            color="secondary"
-                            variant="h6"
-                            gutterBottom
-                          >
-                            {numeral(monthlyPayment).format()} units
-                          </Typography>
-                        </div>
-                        <div className={classes.inlining}>
-                          <Avatar className={classes.interestAvatar}></Avatar>
-                          <Typography
-                            className={classes.inlining}
-                            variant="subtitle2"
-                            gutterBottom
-                          >
-                            Othe type
-                          </Typography>
-                          <Typography
-                            className={classes.inlining}
-                            color="secondary"
-                            variant="h6"
-                            gutterBottom
-                          >
-                            {numeral(monthlyInterest).format()} units
-                          </Typography>
-                        </div>
+                    First title
+                  </Typography>
+                  <Typography variant="body2" gutterBottom>
+                    A first title style <br /> with two lines
+                  </Typography>
+                </div>
+                <div style={{ display: "flex", justifyContent: "flex-end" }}>
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    className={classes.actionButtom}
+                  >
+                    Learn more
+                  </Button>
+                </div>
+              </Paper>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Paper className={classes.paper}>
+                <div className={classes.box}>
+                  <Typography
+                    style={{ textTransform: "uppercase" }}
+                    color="secondary"
+                    gutterBottom
+                  >
+                    Another box
+                  </Typography>
+                  <Typography variant="body1" gutterBottom>
+                    A default box
+                  </Typography>
+                </div>
+                <div style={{ display: "flex", justifyContent: "flex-end" }}>
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    className={classes.actionButtom}
+                  >
+                    Learn more
+                  </Button>
+                </div>
+              </Paper>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Paper className={classes.paper}>
+                <div className={classes.box}>
+                  <Typography
+                    style={{ textTransform: "uppercase" }}
+                    color="secondary"
+                    gutterBottom
+                  >
+                    A box with a carousel
+                  </Typography>
+                  <Typography variant="body1" gutterBottom>
+                    If you click in Getting Started, you will see a nice
+                    carousel
+                  </Typography>
+                </div>
+                <div className={classes.alignRight}>
+                  <Button variant="outlined" className={classes.actionButtom}>
+                    Learn more
+                  </Button>
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    className={classes.actionButtom}
+                  >
+                    Dashboard
+                  </Button>
+                </div>
+              </Paper>
+            </Grid>
+            <Grid container item xs={12}>
+              <Grid item xs={12}>
+                <Paper
+                  className={classes.paper}
+                  style={{ position: "relative" }}
+                >
+                  <div>
+                    <Typography variant="subtitle1" gutterBottom>
+                      Some details
+                    </Typography>
+                    <Typography variant="body1">
+                      Details about the graph
+                    </Typography>
+                    <div style={{ marginTop: 14, marginBottom: 14 }}>
+                      <div className={classes.inlining}>
+                        <Avatar className={classes.loanAvatar}></Avatar>
+                        <Typography
+                          className={classes.inlining}
+                          variant="subtitle2"
+                          gutterBottom
+                        >
+                          Type
+                        </Typography>
+                        <Typography
+                          className={classes.inlining}
+                          color="secondary"
+                          variant="h6"
+                          gutterBottom
+                        >
+                          {numeral(monthlyPayment).format()} units
+                        </Typography>
                       </div>
-                      <div>
-                        <SimpleLineChart data={data} />
+                      <div className={classes.inlining}>
+                        <Avatar className={classes.interestAvatar}></Avatar>
+                        <Typography
+                          className={classes.inlining}
+                          variant="subtitle2"
+                          gutterBottom
+                        >
+                          Othe type
+                        </Typography>
+                        <Typography
+                          className={classes.inlining}
+                          color="secondary"
+                          variant="h6"
+                          gutterBottom
+                        >
+                          {numeral(monthlyInterest).format()} units
+                        </Typography>
                       </div>
                     </div>
-                  </Paper>
-                </Grid>
+                    <div>
+                      <SimpleLineChart data={data} />
+                    </div>
+                  </div>
+                </Paper>
               </Grid>
             </Grid>
           </Grid>
-        </div>
-      </React.Fragment>
-    );
-  }
+        </Grid>
+      </div>
+    </React.Fragment>
+  );
 }
+// }
 
 export default withRouter(withStyles(styles)(Main));
