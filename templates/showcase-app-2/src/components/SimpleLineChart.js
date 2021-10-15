a8b943c0ffeeec6cfad800d2a09203dccba44579
alan green <alangrn@hotmail.com>
2021-10-15 08:08:52 +0100

    upgrade recharts version

--- templates/showcase-app-2/src/components/SimpleLineChart.js
+++ templates/showcase-app-2/src/components/SimpleLineChart.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import ResponsiveContainer from 'recharts/lib/component/ResponsiveContainer';
-import BarChart from 'recharts/lib/chart/BarChart';
-import Bar from 'recharts/lib/cartesian/Bar';
-import XAxis from 'recharts/lib/cartesian/XAxis';
-import Tooltip from 'recharts/lib/component/Tooltip';
-import { withTheme } from '@material-ui/styles';
+import React from "react";
+import { BarChart, Bar, Tooltip, ResponsiveContainer, XAxis } from "recharts";
+import { withTheme } from "@material-ui/styles";
 
 function SimpleLineChart(props) {
   const { theme, data } = props;
   return (
     <ResponsiveContainer width="99%" height={225}>
       <BarChart data={data}>
-        <XAxis dataKey="name"/>
-        <Tooltip/>
+        <XAxis dataKey="name" />
+        <Tooltip />
         <Bar dataKey="Type" stackId="a" fill={theme.palette.primary.main} />
-        <Bar dataKey="OtherType" stackId="a" fill={theme.palette.secondary.light} />
+        <Bar
+          dataKey="OtherType"
+          stackId="a"
+          fill={theme.palette.secondary.light}
+        />
       </BarChart>
     </ResponsiveContainer>
   );
