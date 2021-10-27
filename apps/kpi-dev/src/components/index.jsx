f388bf26e2417a10d502722e5b701e280939ce3c
alan green <alangrn@hotmail.com>
2021-10-27 19:43:15 +0100

    kpi dev

--- apps/kpi-dev/src/components/index.jsx
+++ apps/kpi-dev/src/components/index.jsx
@@ -32,25 +32,11 @@ export { ListGroup, ListGroupItem } from "./ui/list-group/list-group";
 export { Spinner } from "./ui/spinner/spinner";
 export { Media, MediaBody, MediaLeft } from "./ui/media/media";
 export { Navbar } from "./ui/navbar/navbar";
-export {
-  Modal,
-  ModalHeader,
-  ModalTitle,
-  ModalClose,
-  ModalBody,
-  ModalFooter,
-} from "./ui/modal/modal";
+
 export { Container, Row, Col } from "./grid/grid";
 export { default as SectionTitle } from "./ui/section-title";
 export { Table } from "./ui/table/table";
-// export { VectorMap } from "./maps/vector-map/vector-map";
-export {
-  BarChart,
-  HorizontalBarChart,
-  LineChart,
-  PieChart,
-  DonutChart,
-} from "./charts/chart-js/chart-js";
+
 export {
   Input,
   Checkbox,
