e29b77de2369b8d6a83fd2d0e561ac7e79aed8de
alan green <alangrn@hotmail.com>
2021-10-27 19:41:53 +0100

    continued

--- apps/kpi-dev/src/components/index.jsx
+++ apps/kpi-dev/src/components/index.jsx
@@ -1,9 +1,9 @@
 export { ApexCharts } from "apexcharts";
 export { Anchor } from "./ui/anchor";
 export { Avatar, AvatarInitial } from "./ui/avatar/avatar";
-export { Button } from "./ui/button/button";
-export { ButtonGroup, ButtonToolbar } from "./ui/button-group/button-group";
-export { BreadcrumbItem, Breadcrumb } from "./ui/breadcrumb/breadcrumb";
+// export { Button } from "./ui/button/button";
+// export { ButtonGroup, ButtonToolbar } from "./ui/button-group/button-group";
+// export { BreadcrumbItem, Breadcrumb } from "./ui/breadcrumb/breadcrumb";
 export {
   ApexBarChart,
   ApexLineChart,
@@ -28,13 +28,7 @@ export {
   CardColumns,
 } from "./ui/card/card";
 export { ListGroup, ListGroupItem } from "./ui/list-group/list-group";
-export {
-  Dropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  DropdownDivider,
-} from "./ui/dropdown/dropdown";
+
 export { Spinner } from "./ui/spinner/spinner";
 export { Media, MediaBody, MediaLeft } from "./ui/media/media";
 export { Navbar } from "./ui/navbar/navbar";
@@ -49,7 +43,7 @@ export {
 export { Container, Row, Col } from "./grid/grid";
 export { default as SectionTitle } from "./ui/section-title";
 export { Table } from "./ui/table/table";
-export { VectorMap } from "./maps/vector-map/vector-map";
+// export { VectorMap } from "./maps/vector-map/vector-map";
 export {
   BarChart,
   HorizontalBarChart,
