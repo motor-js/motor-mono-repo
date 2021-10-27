03e39eb39c300df73a32ee541e1d09cd57d3549b
alan green <alangrn@hotmail.com>
2021-10-27 15:29:49 +0100

    kpi-dev continued

--- apps/kpi-dev/src/index.js
+++ apps/kpi-dev/src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
+import { Motor } from "@motor-js/engine";
+import { qlikConfig } from "./config";
+import App from "./App";
+import "semantic-ui-css/semantic.min.css";
 
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
+  <Motor config={qlikConfig}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Motor>,
+  rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
