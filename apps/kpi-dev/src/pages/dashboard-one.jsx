43bcba46ecddbdc8af7c2b36ba7f8f3f1adbbf24
alan green <alangrn@hotmail.com>
2021-10-27 18:06:17 +0100

    cleanup

--- apps/kpi-dev/src/pages/dashboard-one.jsx
+++ apps/kpi-dev/src/pages/dashboard-one.jsx
@@ -1,19 +1,16 @@
 import React from "react";
-import { Row } from "../components";
-import Layout from "../layouts";
-import Content from "../layouts/content";
 
 import RowOne from "../containers/dashboard-one/row-one";
 
 const DashboardOne = () => {
   return (
-    <Layout>
-      <Content>
-        <Row gutters={10}>
-          <RowOne />
-        </Row>
-      </Content>
-    </Layout>
+    // <Layout>
+    //   <Content>
+    //     <Row gutters={10}>
+    <RowOne />
+    //     </Row>
+    //   </Content>
+    // </Layout>
   );
 };
 
