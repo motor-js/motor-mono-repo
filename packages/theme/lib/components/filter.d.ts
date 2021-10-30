770441dc4927599f896b8f31a4a70888c1d186f5
lureilly1 <lureilly1@gmail.com>
2021-10-30 15:09:30 +0100

    remove nm

---
+++ packages/theme/lib/components/filter.d.ts
@@ -0,0 +1,21 @@
+export declare type Filter = typeof filter;
+declare const filter: {
+    border: string;
+    borderRadius: string;
+    borderHovered: string;
+    size: {
+        small: {
+            fontSize: string;
+            itemHeight: number;
+        };
+        medium: {
+            fontSize: string;
+            itemHeight: number;
+        };
+        large: {
+            fontSize: string;
+            itemHeight: number;
+        };
+    };
+};
+export default filter;
---
+++ packages/theme/lib/index.d.ts
@@ -0,0 +1,12 @@
+import styled, { ThemeProvider, ThemeContext } from "styled-components";
+import theme from "./theme";
+export declare const device: {
+    small: string;
+    medium: string;
+    large: string;
+    xlarge: string;
+    xxlarge: string;
+};
+export { ThemeProvider, ThemeContext, theme, };
+export * from "styled-system";
+export default styled;
