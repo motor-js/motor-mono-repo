bc00af070b3dd0c32326bbbbeebb195378dd15e1
alan green <alangrn@hotmail.com>
2021-10-30 11:47:31 +0100

    kpi-test setup complete

--- apps/kpi-test/src/fonts/index.ts
+++ apps/kpi-test/src/fonts/index.ts
@@ -1,8 +1,6 @@
 import { IbmFontSans } from "./ibm-plex-sans";
 import { InterUi } from "./inter-ui";
-import { IonIcons } from "./ionicons";
 import { Rubik } from "./rubik";
 import { FontAwesome } from "./font-awesome";
-import { CryptoFont } from "./cryptofont";
 
-export { IbmFontSans, InterUi, IonIcons, Rubik, FontAwesome, CryptoFont };
+export { IbmFontSans, InterUi, Rubik, FontAwesome };
