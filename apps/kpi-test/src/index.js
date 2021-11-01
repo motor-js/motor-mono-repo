6bc23b343c1453a6aa64cb2836a360d01bc11bbc
alan green <alangrn@hotmail.com>
2021-11-01 20:52:02 +0000

    global styling removed

--- apps/kpi-test/src/index.js
+++ apps/kpi-test/src/index.js
@@ -1,17 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Motor } from "@motor-js/engine";
-// import { Provider } from "react-redux";
-import { ThemeProvider, theme } from "./styled";
-import { GlobalStyle } from "./css";
-// import { store } from "./redux/store";
+
 import App from "./App";
 import { qlikConfig } from "./config";
 
 ReactDOM.render(
-  // <Provider store={store}>
-  <ThemeProvider theme={theme}>
-    <GlobalStyle />
+  <>
     <Motor
       config={qlikConfig}
       body={"Please login to get started 🎉 "}
@@ -20,7 +15,7 @@ ReactDOM.render(
     >
       <App />
     </Motor>
-  </ThemeProvider>,
-  // </Provider>
+  </>,
+
   document.getElementById("root")
 );
