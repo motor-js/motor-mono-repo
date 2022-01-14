c2d2ccc0399e23b927c538c55972006e8243119f
alan green <alangrn@hotmail.com>
2022-01-15 01:06:57 +0200

    amCharts iinitial chart

--- apps/amchartswrapper/src/App.js
+++ apps/amchartswrapper/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import AmChartsWrapper from "./components/amChartsWrapper";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <AmChartsWrapper />
       </header>
     </div>
   );
