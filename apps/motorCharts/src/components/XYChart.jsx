8cc68b1924a4e43091c7f0e2a0e15ab33148a838
alan green <alangrn@hotmail.com>
2022-02-07 06:44:32 +0000

    animation added

--- apps/motorCharts/src/components/XYChart.jsx
+++ apps/motorCharts/src/components/XYChart.jsx
@@ -89,6 +89,25 @@ function XYChart(props) {
 
     const { data } = dataSet;
 
+    // To be used e.g. fro axis category type
+    //      const { role } = props
+    //   let component
+    //   switch(role) {
+    //     case 'author':
+    //       component = <AuthorLayout>What will you write today?</AuthorLayout>
+    //     break
+    //     case 'admin':
+    //       component = <AdminLayout>Your latest reports </AdminLayout>
+    //     break
+    //     case 'moderator':
+    //       component = <ModeratorLayout>Your ongoing events</ModeratorLayout>
+    //     break
+    //     default:
+    //       component = <GuestLayout>Your current feed</GuestLayout>
+    //   }
+    //   return component
+    // }
+
     // Define data
     // let data = [
     //   {
