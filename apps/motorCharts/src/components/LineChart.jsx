ee68515c03ffcb064a47ff26b4e7bc2bea737f0f
alan green <alangrn@hotmail.com>
2022-02-12 07:36:56 +0000

    linechart base

---
+++ apps/motorCharts/src/components/LineChart.jsx
@@ -0,0 +1,130 @@
+import React, { useLayoutEffect } from "react";
+
+// https://codesandbox.io/s/6ovk5?file=/src/styles.css:123-206
+// https://codesandbox.io/s/99obk?file=/src/App.js
+
+import * as am5 from "@amcharts/amcharts5";
+import * as am5xy from "@amcharts/amcharts5/xy";
+import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
+import resolveConfig from "tailwindcss/resolveConfig";
+import tailwindConfig from "../tailwind.config";
+import * as am5percent from "@amcharts/amcharts5/percent";
+const { theme } = resolveConfig(tailwindConfig);
+
+// in src type : ln -s ../tailwind.config.js ./
+// console.log(theme.colors);
+
+//chart type
+
+function Pie(props) {
+  //const chart = useRef(null);
+  const chartID = props.chartID;
+  console.log({ chartID });
+
+  useLayoutEffect(() => {
+    //var root = am5.Root.new("chartdiv2");
+    var root = am5.Root.new(chartID);
+
+    root.setThemes([am5themes_Animated.new(root)]);
+
+    // Create chart
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/
+    var chart = root.container.children.push(
+      am5xy.XYChart.new(root, {
+        panX: true,
+        panY: true,
+        wheelX: "panX",
+        wheelY: "zoomX",
+      })
+    );
+
+    // Add cursor
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/cursor/
+    var cursor = chart.set(
+      "cursor",
+      am5xy.XYCursor.new(root, {
+        behavior: "none",
+      })
+    );
+    cursor.lineY.set("visible", false);
+
+    // Generate random data
+    var date = new Date();
+    date.setHours(0, 0, 0, 0);
+    var value = 100;
+
+    function generateData() {
+      value = Math.round(Math.random() * 10 - 5 + value);
+      am5.time.add(date, "day", 1);
+      return {
+        date: date.getTime(),
+        value: value,
+      };
+    }
+
+    function generateDatas(count) {
+      var data = [];
+      for (var i = 0; i < count; ++i) {
+        data.push(generateData());
+      }
+      return data;
+    }
+
+    // Create axes
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
+    var xAxis = chart.xAxes.push(
+      am5xy.DateAxis.new(root, {
+        maxDeviation: 0.2,
+        baseInterval: {
+          timeUnit: "day",
+          count: 1,
+        },
+        renderer: am5xy.AxisRendererX.new(root, {}),
+        tooltip: am5.Tooltip.new(root, {}),
+      })
+    );
+
+    var yAxis = chart.yAxes.push(
+      am5xy.ValueAxis.new(root, {
+        renderer: am5xy.AxisRendererY.new(root, {}),
+      })
+    );
+
+    // Add series
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/series/
+    var series = chart.series.push(
+      am5xy.LineSeries.new(root, {
+        name: "Series",
+        xAxis: xAxis,
+        yAxis: yAxis,
+        valueYField: "value",
+        valueXField: "date",
+        tooltip: am5.Tooltip.new(root, {
+          labelText: "{valueY}",
+        }),
+      })
+    );
+
+    // Add scrollbar
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/scrollbars/
+    chart.set(
+      "scrollbarX",
+      am5.Scrollbar.new(root, {
+        orientation: "horizontal",
+      })
+    );
+
+    // Set data
+    var data = generateDatas(1200);
+    series.data.setAll(data);
+
+    // Make stuff animate on load
+    // https://www.amcharts.com/docs/v5/concepts/animations/
+    series.appear(1000);
+    chart.appear(1000, 100);
+  }, [chartID]);
+
+  // return <div id={chartID} style={{ width: "100%", height: "500px" }}></div>;
+  return <div id={chartID} style={{ width: "100%", height: "500px" }}></div>;
+}
+export default Pie;
