ea1e9f2c4c6694cbe9ddd1b51d7a9c80314a5ce6
alan green <alangrn@hotmail.com>
2022-02-14 04:34:39 +0000

    yRenderer added

--- apps/motorCharts/src/components/helpers/axis.js
+++ apps/motorCharts/src/components/helpers/axis.js
@@ -3,9 +3,18 @@ import * as am5xy from "@amcharts/amcharts5/xy";
 
 export const setYAxis = (chart, root) => {
   // Create Y-axis
+  var yRenderer = am5xy.AxisRendererY.new(root, {});
+  // xRenderer.labels.template.setAll({
+  //   rotation: -45,
+  //   centerY: am5.p50,
+  //   centerX: am5.p100,
+  //   paddingRight: 15,
+  //   // fontSize: "0.5em",
+  //   // fill: am5.color(0x0000ff),
+  // });
   return chart.yAxes.push(
     am5xy.ValueAxis.new(root, {
-      renderer: am5xy.AxisRendererY.new(root, {}),
+      renderer: yRenderer,
     })
   );
 };
@@ -18,6 +27,7 @@ export const setXAxis = (chart, root, dataSet, data) => {
     centerX: am5.p100,
     paddingRight: 15,
     // fontSize: "0.5em",
+    // fill: am5.color(0x0000ff),
   });
   // let xRenderer = xAxis.get("renderer");
   // xRenderer.labels.template.setAll({
