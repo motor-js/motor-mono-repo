e959f25e5976d4126f2358118bb7ab44856bfdc4
alan green <alangrn@hotmail.com>
2022-02-14 06:30:37 +0000

    tooltip placeholder

---
+++ apps/motorCharts/src/components/helpers/tooltips.js
@@ -0,0 +1,16 @@
+// https://www.amcharts.com/docs/v5/concepts/common-elements/tooltips/
+
+// let tooltip = am5.Tooltip.new(root, {
+//   getFillFromSprite: false,
+//   getStrokeFromSprite: true,
+//   autoTextColor: false,
+//   getLabelFillFromSprite: true,
+//   labelText: "[bold]{name}[/]\n{valueX.formatDate()}: {valueY}",
+// });
+
+// tooltip.get("background").setAll({
+//   fill: am5.color(0xffffff),
+//   fillOpacity: 0.8,
+// });
+
+// series.set("tooltip", tooltip);
