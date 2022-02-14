9eb5815d1aa6ae25b01935d9738fa654fed56a3d
alan green <alangrn@hotmail.com>
2022-02-14 06:07:26 +0000

    label placeholder

--- apps/motorCharts/src/components/helpers/labels.js
+++ apps/motorCharts/src/components/helpers/labels.js
@@ -1,1 +1,14 @@
 // https://www.amcharts.com/docs/v5/concepts/common-elements/labels/
+
+// chart.children.unshift(
+//   am5.Label.new(root, {
+//     text: "This is a chart title",
+//     fontSize: 25,
+//     fontWeight: "500",
+//     textAlign: "center",
+//     x: am5.percent(50),
+//     centerX: am5.percent(50),
+//     paddingTop: 0,
+//     paddingBottom: 0,
+//   })
+// );
